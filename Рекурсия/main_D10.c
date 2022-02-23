94640f11ba8071f380e0601dfd9ee398dec9a19a
IvanR <lohmatysh@gmail.com>
2022-02-23 17:18:20 +0300

    new

---
+++ Рекурсия/main_D10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <inttypes.h>
+
+int is_prime(int n, int delitel);
+
+int main() {
+    int n = 0, delitel = 1;
+    scanf("%d", &n);
+    is_prime(n, delitel);
+    return 0;
+} 
+
+int is_prime(int n, int delitel) {
+
+    return 0;    
+}
