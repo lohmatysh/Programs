67d3c7006f43788c1abdcfefb711dc3cbc5a2db4
IvanR <lohmatysh@gmail.com>
2022-03-12 22:28:08 +0300

    new

--- Массивы/main_E14.c
+++ Массивы/main_E14.c
@@ -2,23 +2,40 @@
 #include <stdbool.h>
 #include <inttypes.h>
 
+void swap(int *pa, int *pb);
+
 int main() {
     enum {SIZE = 10};
     int array [SIZE] = {0};
-    int digits [SIZE] = {0};
-    int i = 0, j = 0, k = 0;
+    int array_sort [SIZE] = {0};
+    int i = 0, j = 0, k = 0, last_num = -1;
     for (i = 0; i < SIZE; i++) {
         scanf("%d", &array [i]);
     }
     for (j = 0; j < SIZE - 1; j++) {
-        for (k = j + 1; k < SIZE; k++) {
-            if (array [j] == array [k]) {
-                digits [j]++;
+        for (k = 0; k < SIZE - 1 - j; k++) {
+                if (array [k] > array [k + 1]) {
+                    swap(&array [k], &array [k + 1]);
+                }
             }
-        }
     }
-    for (i = 0; i < SIZE; i++) {
-        printf("%d ", digits [i]);;
+    j = 0;
+    for (i = 0; i < SIZE - 1; i++) {
+        if (array [i] == array [i + 1] && array [i] != last_num) {
+            array_sort [j] = array [i];
+            j++;
+        }
+        last_num = array [i];
+    }
+    for (i = 0; i < j; i++) {
+        printf("%d ", array_sort [i]);
     }
     return 0;
+}
+
+void swap(int *pa, int *pb) {
+    int tmp = 0;
+    tmp = *pa;
+    *pa = *pb;
+    *pb = tmp;
 }
