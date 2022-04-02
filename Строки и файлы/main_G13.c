0646885c4b890a17edcab71a53bcb34077b2a3ab
IvanR <lohmatysh@gmail.com>
2022-04-03 00:11:59 +0300

    new

--- Строки и файлы/main_G13.c
+++ Строки и файлы/main_G13.c
@@ -6,17 +6,27 @@ int main() {
     FILE *f;
     char string [1200] = {0};
     char c = 0;
-    int i = 0, size = 0, check_dot = 0;
+    int i = 0, size = 0, check_dot = 0, check_end = 0;
     f = fopen("input.txt", "r");
     while ((c = fgetc(f)) != EOF && c != '\n') {
         string [i] = c;
         if (string [i] == '.') {
             check_dot = i;
+            check_end = 1;
+        }
+        if (check_end == 1) {
+            if ((string [i] >= 'a' && string [i] <= 'z') || (string [i] == '.')) {
+                //printf ("%c", string [i]);
+                check_end = 1;
+            }
+            else {
+               check_end = 0;
+            }
         }
         i++;
     }
     size = i;
-    if ((size - check_dot) > 5) {
+    if ((size - check_dot) > 5 && !(check_end)) {
         string [size] = '.';
         check_dot = size;
         size++;
