df4ff16e1ac774fd5483dc684ef1d841135a7a4e
IvanR <lohmatysh@gmail.com>
2022-05-06 17:21:43 +0300

    new

--- Курсовая/temp_functions.h
+++ Курсовая/temp_functions.h
@@ -3,9 +3,9 @@
 #include <unistd.h>
 
 // Глобальные переменные
-_Bool check_month = false, check_year = false;
+_Bool check_month, check_year;
 char file_name[256];
-int a, y, m, d, h, mi, t, rez = 0, nm = 0;
+int a, y, m, d, h, mi, t, rez, nm;
 FILE *f;
 
 // Структуры и объединения
