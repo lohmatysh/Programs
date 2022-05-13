6dbba2c03327bf0c78e37bd330e0bc892de3624e
IvanR <lohmatysh@gmail.com>
2022-05-13 15:40:48 +0300

    new

--- Строки и файлы/main_G8.c
+++ Строки и файлы/main_G8.c
@@ -2,12 +2,14 @@
 #include <stdbool.h>
 #include <inttypes.h>
 
+void swap(int *pa, int *pb);
+
 int main() {
     FILE *f;
-    char string [1000] = {0};
-    int array_nums[1000] = {0};
+    char string [1001] = {0};
+    int array_nums[1001] = {0};
     char c = 0;
-    int i = 0, j = 0, k = 0, factor = 0, mult = 1, current = 0, tmp = 0;
+    int i = 0, j = 0, k = 0, factor = 0, mult = 1, current = 0;
     f = fopen("input.txt", "r");
     while ((c = fgetc(f)) != EOF && c != '\n') {
         string [i] = c;
@@ -35,16 +37,23 @@ int main() {
             mult = 1;
         }
     }
-    for (int i = 0; i < k - 1; i++) {
-        if (array_nums[i + 1] < array_nums[i]) {
-            tmp = array_nums[i+1];
-            array_nums[i + 1] = array_nums[i];
-            array_nums[i] = tmp;
+    for (int j = 0; j < k - 1; j++) {
+        for (int m = 0; m < k - 1; m++) {
+            if (array_nums[m + 1] < array_nums[m]) {
+                swap(&array_nums[m], &array_nums[m + 1]);
+            }
         }
     }
     for (int i = 0; i < k; i++) {
-        fprintf(f, "%d ", array_nums[i]);
+        printf("%d ", array_nums[i]);
     }
     fclose (f);
     return 0;  
+}
+
+void swap(int *pa, int *pb) {
+    int tmp = 0;
+    tmp = *pa;
+    *pa = *pb;
+    *pb = tmp;
 }
