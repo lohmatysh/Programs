985ae513d35d2d018f46431dccb2b40877688337
IvanR <lohmatysh@gmail.com>
2022-07-14 09:37:46 +0300

    new

--- Курсовая/temp_functions.h
+++ Курсовая/temp_functions.h
@@ -24,11 +24,15 @@ union u {
 float average_value(struct temperature* month, int n);
 int max_value(struct temperature* month, int n, int t);
 int min_value(struct temperature* month, int n, int t);
+
 float average_value_year(struct temperature* month);
 int max_value_year(struct temperature* month);
 int min_value_year(struct temperature* month);
+
 void print_space();
 void print_info();
+
 int scan_keys(int argc, char *argv[], char *file_name, union u union_month, int rez, int nm);
 int scan_data(struct temperature* month, FILE *f, char *file_name, int a, int y, int m, int d, int h, int mi, int t);
+
 void print_stats(struct temperature* month, int check, int error);
